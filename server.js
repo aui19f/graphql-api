a84397a67dc7de56b3edf379948ed82121bc6e5f
aui19f <aui19f@gmail.com>
2024-09-29 13:01:35 +0900

    rest to graphql

--- server.js
+++ server.js
@@ -23,10 +23,36 @@ const typeDefs = gql`
     text: String
     author: User
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
+
   type Query {
     allTweets: [Tweet]
     tweet(id: ID): Tweet
     allUsers: [User]
+    allMovies: [Movie]
+    movie(id: ID): Movie
   }
   type Mutation {
     postTweet(text: String, userId: ID): Tweet
@@ -49,6 +75,16 @@ const resolvers = {
     },
     allUsers() {
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((res) => res.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((res) => res.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
